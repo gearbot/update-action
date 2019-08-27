342606600d0151e699b716a7ceffcd9d0f40e98f
AEnterprise <aenterprise2@gmail.com>
2019-08-27 17:56:44 +0200

    determine update type

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,31 @@ import * as core from '@actions/core';
 import * as github from '@actions/github';
 
 async function run() {
-  try {
-    // const myInput = core.getInput('myInput');
-    // core.debug(`Hello ${myInput}`);
-    core.debug(JSON.stringify(github.context.payload))
-  } catch (error) {
-    core.setFailed(error.message);
-  }
+    try {
+        const commits = github.context.payload.commits;
+        let output = "update";
+        if (commits.length >= 20)
+            output = "upgrade";
+        else {
+            const files = new Set();
+            for (let c in commits) {
+                const commit = commits[c];
+                // @ts-ignore
+                commit.modified.forEach(files.add);
+            }
+
+            const upgradeFiles = [
+                "GearBot/GearBot.py",
+                "GearBot/Bot/GearBot.py",
+                "GearBot/Util/PromMonitors.py"
+            ];
+            if (upgradeFiles.filter(f => files.has(f)).length > 0)
+                output = "upgrade"
+        }
+        core.exportVariable("UPDATE_TYPE", output);
+    } catch (error) {
+        core.setFailed(error.message);
+    }
 }
 
 run();
