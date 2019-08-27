f6862d3defc18a6de52cfd69f4eb4d417fab537e
AEnterprise <aenterprise2@gmail.com>
2019-08-27 17:20:01 +0200

    just some debug logging for now

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import * as core from '@actions/core';
+import * as github from '@actions/github';
 
 async function run() {
   try {
-    const myInput = core.getInput('myInput');
-    core.debug(`Hello ${myInput}`);
+    // const myInput = core.getInput('myInput');
+    // core.debug(`Hello ${myInput}`);
+    core.debug(JSON.stringify(github.context.payload))
   } catch (error) {
     core.setFailed(error.message);
   }
